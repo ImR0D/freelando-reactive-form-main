84f71c89e36c20e52d62055e45ba2f5b2c4f129c
Rodrigo Ferreira <rodrigo.ferreira@pca.com.br>
2026-08-06 16:18:46 -0300

    feat: implementado formulário reativo para o formulário de cadastro com as seções 'area de atuação' e 'nível de experiência'

--- src/app/pages/cadastro-form/cadastro-form.component.ts
+++ src/app/pages/cadastro-form/cadastro-form.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RadioOptionComponent } from '../../shared/components/radio-option/radio-option.component';
+import { ExperienceLevelComponent } from '../../shared/components/experience-level/experience-level.component';
+import { ButtonComponent } from '../../shared/components/button/button.component';
 
-const MODULES = [
-  CommonModule,
-  ReactiveFormsModule
+const MODULES = [CommonModule, ReactiveFormsModule];
+const COMPONENTS = [
+  RadioOptionComponent,
+  ExperienceLevelComponent,
+  ButtonComponent,
 ];
 
 @Component({
   selector: 'app-cadastro-form',
   standalone: true,
-  imports: [
-    ...MODULES
-  ],
+  imports: [...MODULES, ...COMPONENTS],
   templateUrl: './cadastro-form.component.html',
-  styleUrls: ['./cadastro-form.component.scss']
+  styleUrls: ['./cadastro-form.component.scss'],
 })
-export class CadastroFormComponent {
+export class CadastroFormComponent implements OnInit {
   cadastroForm!: FormGroup;
 
   areasAtuacao = [
@@ -25,24 +33,49 @@ export class CadastroFormComponent {
     { id: 'revisao', value: 'revisao', label: 'Revisão' },
     { id: 'traducao', value: 'traducao', label: 'Tradução' },
     { id: 'transcricao', value: 'transcricao', label: 'Transcrição' },
-    { id: 'marketing', value: 'marketing', label: 'Marketing' }
+    { id: 'marketing', value: 'marketing', label: 'Marketing' },
   ];
 
   niveisExperiencia = [
     {
       id: 'iniciante',
       label: 'Iniciante',
-      description: '(1 a 3 anos)'
+      description: '(1 a 3 anos)',
     },
     {
       id: 'intermediario',
       label: 'Intermediário',
-      description: '(3 a 6 anos)'
+      description: '(3 a 6 anos)',
     },
     {
       id: 'avancado',
       label: 'Avançado',
-      description: '(6 anos ou mais)'
-    }
+      description: '(6 anos ou mais)',
+    },
   ];
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.cadastroForm = this.formBuilder.group({
+      areasAtuacao: ['', Validators.required],
+      niveisExperiencia: ['', Validators.required],
+    });
+  }
+
+  onPrevious() {
+    console.log('Voltar para a etapa anterior');
+  }
+
+  onNext() {
+    if (this.cadastroForm.valid) {
+      console.log('Formulário válido: ', this.cadastroForm);
+    }
+  }
+  onAreaChange(area: string) {
+    this.cadastroForm.get('areasAtuacao')?.setValue(area);
+  }
+  onLevelChange(level: string) {
+    this.cadastroForm.get('niveisExperiencia')?.setValue(level);
+  }
 }
